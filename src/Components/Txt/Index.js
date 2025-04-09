bb110b19d61886821c3e977951fe53650d575965
Ather Ali Siddiqui <engr.ather.93@gmail.com>
2025-04-09 15:12:30 +0500

    edit Inputfeild component

--- src/Components/Txt/Index.js
+++ src/Components/Txt/Index.js
@@ -1,7 +1,6 @@
 import { Text } from 'react-native';
 import React from 'react';
 import Typography from '../../Constants/Typography';
-import Typography from '../../Constants/Typography';
 
 const Txt = ({
   size = 24,
