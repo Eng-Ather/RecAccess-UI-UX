6e4f404a0d1776b2cd0def82c0a63f0a03b83ead
Ather Ali Siddiqui <engr.ather.93@gmail.com>
2025-04-09 15:10:00 +0500

    add inputfeild component

--- src/Components/Txt/Index.js
+++ src/Components/Txt/Index.js
@@ -1,6 +1,7 @@
 import { Text } from 'react-native';
 import React from 'react';
 import Typography from '../../Constants/Typography';
+import Typography from '../../Constants/Typography';
 
 const Txt = ({
   size = 24,
